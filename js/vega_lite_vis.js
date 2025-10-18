6f001fb0746304cafc0d5f93913ae531f478fbee
rvya1602 <126407677+rvya1602@users.noreply.github.com>
2025-10-19 04:55:59 +1100

    Added area chart - next bubble chart for in demand degrees

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -12,3 +12,8 @@ var vg_3 = "js/total_students_by_state_uni.vg.json";
 // { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
 vegaEmbed("#total_students_state_uni", vg_3, { actions : false }).then(function(result) {
 }).catch(console.error);
+
+var vg_4 = "js/dom_vs_int_students.vg.json";
+// { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
+vegaEmbed("#dom_vs_int_students", vg_4, { actions : false }).then(function(result) {
+}).catch(console.error);
