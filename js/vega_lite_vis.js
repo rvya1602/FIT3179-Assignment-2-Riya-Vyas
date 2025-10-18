371d2245875ecf80554076502352853904a5e017
rvya1602 <126407677+rvya1602@users.noreply.github.com>
2025-10-19 02:54:49 +1100

    Added graph for commencing vs total students by state and institution

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,14 +1,14 @@
-// var vg_1 = "js/barchart.vg.json";
-// // { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
-// vegaEmbed("#barchart", vg_1, { actions : false }).then(function(result) {
-// }).catch(console.error);
-
-var vg_2 = "js/int_students_by_cob_2024.vg.json";
+var vg_1 = "js/int_students_by_cob_2024.vg.json";
 // { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
-vegaEmbed("#int_students_by_cob", vg_2, { actions : false }).then(function(result) {
+vegaEmbed("#int_students_by_cob", vg_1, { actions : false }).then(function(result) {
 }).catch(console.error);
 
-var vg_3 = "js/bach_or_higher_by_state.vg.json";
+var vg_2 = "js/bach_or_higher_by_state.vg.json";
 // { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
-vegaEmbed("#pop_degree_by_state", vg_3, { actions : false }).then(function(result) {
+vegaEmbed("#pop_degree_by_state", vg_2, { actions : false }).then(function(result) {
+}).catch(console.error);
+
+var vg_3 = "js/total_students_by_state_uni.vg.json";
+// { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
+vegaEmbed("#total_students_state_uni", vg_3, { actions : false }).then(function(result) {
 }).catch(console.error);
