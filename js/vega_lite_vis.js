e6eb268577065c54ce4d7565a4d835be41c932b4
rvya1602 <126407677+rvya1602@users.noreply.github.com>
2025-10-18 23:37:53 +1100

    Initial graph - barchart and map

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,14 @@
+// var vg_1 = "js/barchart.vg.json";
+// // { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
+// vegaEmbed("#barchart", vg_1, { actions : false }).then(function(result) {
+// }).catch(console.error);
+
+var vg_2 = "js/int_students_by_cob_2024.vg.json";
+// { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
+vegaEmbed("#int_students_by_cob", vg_2, { actions : false }).then(function(result) {
+}).catch(console.error);
+
+var vg_3 = "js/bach_or_higher_by_state.vg.json";
+// { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
+vegaEmbed("#pop_degree_by_state", vg_3, { actions : false }).then(function(result) {
+}).catch(console.error);
