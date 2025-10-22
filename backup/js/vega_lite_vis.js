7fac4237ce145c396ee19b32f78ab772ecea51da
rvya1602 <126407677+rvya1602@users.noreply.github.com>
2025-10-23 01:50:09 +1100

    first 2 graphs layout done

---
+++ backup/js/vega_lite_vis.js
@@ -0,0 +1,34 @@
+var vg_1 = "js/int_students_by_cob_2024.vg.json";
+// { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
+vegaEmbed("#int_students_by_cob", vg_1, { actions : false }).then(function(result) {
+}).catch(console.error);
+
+var vg_2 = "js/bach_or_higher_by_state.vg.json";
+// { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
+vegaEmbed("#pop_degree_by_state", vg_2, { actions : false }).then(function(result) {
+}).catch(console.error);
+
+var vg_3 = "js/total_students_by_state_uni.vg.json";
+// { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
+vegaEmbed("#total_students_state_uni", vg_3, { actions : false }).then(function(result) {
+}).catch(console.error);
+
+var vg_4 = "js/dom_vs_int_students.vg.json";
+// { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
+vegaEmbed("#dom_vs_int_students", vg_4, { actions : false }).then(function(result) {
+}).catch(console.error);
+
+var vg_5 = "js/dom_com_by_edu_2015_2024.vg.json";
+// { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
+vegaEmbed("#dom_com_edu_2015_2024", vg_5, { actions : false }).then(function(result) {
+}).catch(console.error);
+
+var vg_6 = "js/dom_com_by_edu_2023_2024.vg.json";
+// { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
+vegaEmbed("#dom_com_edu_2023_2024", vg_6, { actions : false }).then(function(result) {
+}).catch(console.error);
+
+var vg_7 = "js/undergrad_emp_sal_2023.vg.json";
+// { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
+vegaEmbed("#undergrad_emp_sal", vg_7, { actions : false }).then(function(result) {
+}).catch(console.error);
